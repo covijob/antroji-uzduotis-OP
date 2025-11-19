7ea400c37ca5f152d0b5cc842dee535aa120397a
MangirdasG <62077619+covijob@users.noreply.github.com>
2025-11-19 14:14:20 +0200

    struct Studentas pakeistas i class

--- studentai.hpp
+++ studentai.hpp
@@ -2,9 +2,44 @@
 #include <string>
 #include <vector>
 
-struct Studentas {
+#pragma once
+#include <string>
+#include <vector>
+
+class Studentas {
+public:
+    Studentas()
+        : vardas{}, pavarde{}, nd{}, egzaminas{ 0 } {
+    }
+
+    Studentas(const std::string& vardas,
+        const std::string& pavarde,
+        const std::vector<int>& nd,
+        int egzaminas)
+        : vardas{ vardas }, pavarde{ pavarde }, nd{ nd }, egzaminas{ egzaminas } {
+    }
+
+    Studentas(const Studentas& other)
+        : vardas{ other.vardas },
+        pavarde{ other.pavarde },
+        nd{ other.nd },
+        egzaminas{ other.egzaminas } {
+    }
+
+    Studentas& operator=(const Studentas& other) {
+        if (this != &other) {
+            vardas = other.vardas;
+            pavarde = other.pavarde;
+            nd = other.nd;
+            egzaminas = other.egzaminas;
+        }
+        return *this;
+    }
+
+    ~Studentas() = default;
+
     std::string vardas;
     std::string pavarde;
     std::vector<int> nd;
-    int egzaminas = 0;
+    int egzaminas;
 };
