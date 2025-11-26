e88bb5822b4224e64399cf584d138142c9a778d4
MangirdasG <62077619+covijob@users.noreply.github.com>
2025-11-26 19:59:37 +0200

    abstrakti klase Zmogus pilnai baigta.

--- skaiciavimas.hpp
+++ skaiciavimas.hpp
@@ -2,8 +2,5 @@
 #include <vector>
 #include "studentai.hpp"
 
-double vidurkis(const std::vector<int>& pazymiai);
-double mediana(std::vector<int> pazymiai);
-
 double galutinis_vidurkis(const Studentas& s);
 double galutinis_mediana(const Studentas& s);
