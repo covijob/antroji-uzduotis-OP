57578349ae1ebe3a4ba83ac7b59254e9eacd8669
MangirdasG <62077619+covijob@users.noreply.github.com>
2025-12-11 17:35:40 +0200

    error taisymas ir pirmasis unit test igyvendinimas.

--- unit_tests.cpp
+++ unit_tests.cpp
@@ -1,16 +1,24 @@
-#define CATCH_CONFIG_RUNNER
-
+﻿#define CATCH_CONFIG_RUNNER
 #include "catch_amalgamated.hpp"
+
 #include "studentai.hpp"
 #include "skaiciavimas.hpp"
 #include "sort.hpp"
 #include <vector>
 #include <algorithm>
 #include <iostream>
-
+#include <cmath>
 
 int run_all_tests() {
+    Catch::Session session;
+    return session.run();
+}
 
-	std::cout << "test";
-	return 0;
+// ===== TESTAI =====
+
+TEST_CASE("Studento vidurkis apskaiciuojamas teisingai") {
+    SECTION("Keli ND pazymiai") {
+        Studentas s("Vardas", "Pavarde", { 10, 8, 6 }, 9);
+        REQUIRE(s.vidurkis() == Catch::Approx(8.0));
+    }
 }
